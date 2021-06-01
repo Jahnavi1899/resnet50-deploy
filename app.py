f599c8d0a9102891a1dc457b4b8765093ba8551c
Jahnavi <jahnaviraman186@gmail.com>
2021-06-01 16:03:08 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,61 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from werkzeug.utils import secure_filename
+import numpy as np
+
+
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+IMAGE_SIZE = (224,224 )  ## Based on the file size
+UPLOAD_FOLDER = 'static/uploads'
+#model = load_model('color_model.h5')  ## Upload the saved model
+model = ResNet50(weights='imagenet')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def predict(file):
+    img  = load_img(file, target_size=IMAGE_SIZE)
+    img = img_to_array(img)  #/255.0
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    prediction = model.predict(img)
+    #pred = np.argmax(prediction, axis=1)
+    label = decode_predictions(prediction, top=1)[0]
+    return label
+
+
+app = Flask(__name__, template_folder='Templates')  ## To upload files to folder
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/")
+def template_test():
+    return render_template('home.html', label='', imagesource='file://null')
+
+
+@app.route('/', methods=['GET', 'POST'])  ## Main post and get methods for calling and getting a response from the server
+def upload_file(): 
+    if request.method == 'POST':
+        file = request.files['file']
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            output = predict(file_path)
+    return render_template("home.html", label=output, imagesource=file_path)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
